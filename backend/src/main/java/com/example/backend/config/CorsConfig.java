46ba26ffecd76e61a3cf5877a2a27e1458704ed6
sakthivel182006 <sakthivelv202222@gmail.com>
2026-03-15 01:19:52 +0530

    vnsdjvnskvsvdsvdsvs updaeted the cors permission j

--- backend/src/main/java/com/example/backend/config/CorsConfig.java
+++ backend/src/main/java/com/example/backend/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(false);
